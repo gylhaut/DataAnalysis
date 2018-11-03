aefb90007f8eac1fba237360131c34b16416ac33
gylhaut <842560162@qq.com>
2018-11-04 01:30:37 +0800

    gylhaut commit

---
+++ BostonHousing/__init__.py
@@ -0,0 +1,23 @@
+
+"""
+@version: ??
+@author: gylhaut
+@license: Apache Licence 
+@contact: hnsqgyl@163.com
+@site: http://www.cnblogs.com/gylhaut/
+@software: PyCharm
+@file: __init__.py.py
+@time: 2018/11/03 下午 08:46
+"""
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ BostonHousing/boston_housing_keras.py
@@ -0,0 +1,9 @@
+from keras.datasets import boston_housing
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import RMSprop
+import numpy as np
+
+
+if __name__ == '__main__':
+    pass
